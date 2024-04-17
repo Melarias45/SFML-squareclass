3a35651d8fe190fbda37d015a074af2f724f938f
Mel <22073@portalcedi.edu.mx>
2024-04-17 13:53:24 -0600

    1er cambio

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
+
+float speedX =5.f; 
+float speedY =5.f; 
+
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
+    RenderWindow window(VideoMode(800, 600), "SFML works!");
+    RectangleShape shape({50.f, 30.f}); //le pasamos un vector con variables tipo float, vector 2F es que hay 2 dimensiones. 
+    shape.setFillColor(Color::Magenta);
+    window.setFramerateLimit(60); //para que no se mueva tan rapido, porque la compu ejecuta muchisimos frames por segundo de texto. 
 
     while (window.isOpen())
     {
@@ -15,7 +20,12 @@ int main()
                 window.close();
         }
 
-        window.clear();
+        window.clear(); //si podemos antes lo de mover, se va a ver como barrido y no irse borrando para dar la ilusion de que se esta moviendo la figura. 
+        if (shape.getPosition().x+shape.getSize().x>=800||shape.getPosition().x<0)
+        speedX*=-1; 
+        if (shape.getPosition().y+shape.getSize().y>=600||shape.getPosition().y<0)
+        speedY*=-1; 
+        shape.move({speedX,speedY}); //para mover figura en x y en y 
         window.draw(shape);
         window.display();
     }
