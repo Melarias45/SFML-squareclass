82cbe21cbbef8c60204494b05a872f1a945c4be1
Mel <22073@portalcedi.edu.mx>
2024-04-24 13:21:43 -0600

    1

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ Vector2f speed={5.f,5.f};
 int main()
 {
     RenderWindow window(VideoMode(800, 600), "SFML works!");
-    Rectangle shape (Vector2f(50.f,50.f)); 
     window.setFramerateLimit(60); //para que no se mueva tan rapido, porque la compu ejecuta muchisimos frames por segundo de texto. 
+    Rectangle shape (Vector2f(50.f,50.f)); 
 
     while (window.isOpen())
     {
@@ -22,9 +22,9 @@ int main()
 
         // window.clear(); //si podemos antes lo de mover, se va a ver como barrido y no irse borrando para dar la ilusion de que se esta moviendo la figura. 
         
-        window.clear(); 
-        shape.update(); 
-        shape.drawTo(window); 
+        window.clear(); //si se quita quedaria un rastro de la figura. 
+        shape.update(); //actualiza
+        shape.drawTo(window); //dibuja la ventana 
         window.display();
     }
 
