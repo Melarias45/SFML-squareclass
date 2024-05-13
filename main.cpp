0260d421316e39927cf62992cb11f7ef159f0f68
Mel <22073@portalcedi.edu.mx>
2024-05-13 13:25:17 -0600

    en blanco

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <SFML/Graphics.hpp>
-#include "rectangle.hpp"
+
+#include <iostream>
 using namespace sf;
-
-
-Vector2f speed={5.f,5.f};
 
 int main()
 {
     RenderWindow window(VideoMode(800, 600), "SFML works!");
-    window.setFramerateLimit(60); //para que no se mueva tan rapido, porque la compu ejecuta muchisimos frames por segundo de texto. 
-    Rectangle shape (Vector2f(50.f,50.f)); 
+    window.setFramerateLimit(60); 
 
     while (window.isOpen())
     {
@@ -20,11 +17,9 @@ int main()
                 window.close();
         }
 
-        // window.clear(); //si podemos antes lo de mover, se va a ver como barrido y no irse borrando para dar la ilusion de que se esta moviendo la figura. 
+       
         
-        window.clear(); //si se quita quedaria un rastro de la figura. 
-        shape.update(); //actualiza
-        shape.drawTo(window); //dibuja la ventana 
+        window.clear(); 
         window.display();
     }
 
